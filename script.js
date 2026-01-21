6da90f4029da6d9ecb1f3a748dd9986695fce2ea
kuwayamasfida <kuwayamasfida@gmail.com>
2026-01-21 15:21:31 +0900

    Display posts as cards from local storage
    Add functionality to display posts from local storage as cards.

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ document.getElementById("heroBtn").addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+
+const carList = document.getElementById("carList");
+const posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+posts.forEach(post => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <div class="image-box">
+      ${post.img ? `<img src="${post.img}">` : "NO IMAGE"}
+    </div>
+    <h3>${post.name}</h3>
+    <p>${post.desc}</p>
+  `;
+
+  carList.appendChild(card);
+});
